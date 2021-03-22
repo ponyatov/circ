b7c25b90472e70bc8e3dbf90d48cfca4edd256ed
Dmitry Ponyatov <dponyatov@gmail.com>
2021-03-22 08:54:05 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- metaL.py
+++ metaL.py
@@ -209,10 +209,16 @@ README \
     // '## Idea' \
     // '' \
     // '* take Lisp homoiconic nature and port it to Python stack (VM & libs)' \
-    // '* provide a light environment for generative metaprogramming (scripted code generation)' \
+    // '* provide light environment for generative metaprogramming (scripted code generation)' \
     // '  * writing programs that write other programs' \
     // '  * system bootstrap via metacircular definition' \
     // '  * automated source code generation for typical tasks' \
+    // '* integrate best features from multiple languages:' \
+    // '  * `Python` dynamics, readable syntax, lib batteries and ease of use' \
+    // '  * `Erlang`/`Elixir` network-oriented programming: fault tolerance, fast light VM,' \
+    // '    transparent clustering, bit arrays (for IoT)' \
+    // '  * `Clojure` homoiconic extendable language' \
+    // '  * `Smalltalk` message-based OOP' \
     // '' \
     // '## Links' \
     // '' \
